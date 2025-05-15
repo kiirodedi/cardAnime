85f9426b8766ec73ca1408d5d6bb1d14cfdd2cfa
kiirodedi <moraismoralesdeborafernanda3@gmail.com>
2025-05-15 14:53:40 -0300

    2 alteração

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { ScrollView } from 'react-native-web';
+
 import { CardAnime } from "./components/cardAnime.js"
 import personagens from "./data/personagens.json"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {
-        personagens.map((personagem) => (
+      <View style={{ alignItems: 'center' }}>
+        <Text style={styles.titulo}>Card Anime</Text>
+        <ScrollView contentContainerStyle={styles.scrollContent}>
+          {personagens.map((personagem) => (
           <CardAnime
             key={personagem.id}
-            id = {personagem.id}
-            imagem = {personagem.imagem}
-            universo = {personagem.universo}
+            id={personagem.id}
+            imagem={personagem.imagem}
+            nome={personagem.nome}
+            universo={personagem.universo}
           />
-        ))
-      }
+          ))}
+        </ScrollView>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -24,8 +30,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#303030',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
   },
+
+  titulo: {
+    backgroundColor: "#c00277",
+    width: "100%",
+    height: 50,
+    textAlign: "center",
+    marginBottom: 20,
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+
+  scrollContent: {
+  alignItems: 'center',
+  paddingBottom: 30,
+},
 });
--- components/cardAnime.js
+++ components/cardAnime.js
@@ -1,17 +1,57 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export function CardAnime({
     id = "0",
-    imagem,
+    imagem = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtz9VybUWGNZp8DdMSUjmu7MpR6w_ckDHFjg&s",
     nome = "Não informado",
     universo = "Não informado"
 }) {
     return (
-        <View>
-            <Text>Id: {id}</Text>
-            <Image style={estilo.img} source={{ uri: imagem }}></Image>
-            <Text>Nome: {nome}</Text>
-            <Text>Universo: {universo}</Text>
+        <View style={estilo.container}>
+            <View style={estilo.cardPersonagem}>
+                <Image style={estilo.img} source={{ uri: imagem }}></Image>
+                <Text style={estilo.nomePersonagem}>{nome}</Text>
+                <Text style={estilo.universo}>{universo}</Text>
+            </View>
         </View>
     )
 }
+
+const estilo = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: "#303030",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    img: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10
+  },
+
+  cardPersonagem:{
+    backgroundColor: "#1d1d1d",
+    color: "#fff",
+    padding: 10,
+    marginBottom: 30,
+    borderRadius: 10,
+  },
+
+  nomePersonagem:{
+    color: "#fff",
+    backgroundColor: "#c00277",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: 5,
+  },
+
+  universo:{
+    color: "#fff",
+    fontSize: 16,
+    textAlign: "center",
+  }
+});
